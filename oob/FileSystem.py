b401e9892a6c11633473f97f7fd1883ca1930e68
janlingen <jan-lingen@outlook.com>
2021-12-28 23:32:01 +0100

    summary of some learnings

--- oob/FileSystem.py
+++ oob/FileSystem.py
@@ -1,48 +1,128 @@
-import random as r
-
-class Deck:
-    suits = ["H", "D", "C", "S"]
-    values = [str(i) for i in range(2, 11)] + ["J", "Q", "K", "A"]
-
+class FileSystem:
     def __init__(self):
-        self.cards = []
-        for suit in Deck.suits:
-            for value in Deck.values:
-                self.cards.append(value+suit)
+        self.root = Directory("/")
+
+    def create_directory(self, path):
+        FileSystem._validate_path(path)
+        dirs = path[1:].split("/")
+        bottom = self._find_bottom_node(dirs[:-1])
+        if dirs[-1] not in bottom.children:
+            new_dir = Directory(dirs[-1])
+            bottom.add_node(new_dir)
+        else:
+            raise ValueError("Dir already exists")
+
+
+    def create_file(self, path, contents):
+        FileSystem._validate_path(path)
+        dirs = path[1:].split("/")
+        bottom = self._find_bottom_node(dirs[:-1])
+        if dirs[-1] not in bottom.children:
+            new_file = File(dirs[-1])
+            new_file.write_contents(contents)
+            bottom.add_node(new_file)
+        else:
+            raise ValueError("File already exists")
+
+    def read_file(self, path):
+        FileSystem._validate_path(path)
+        dirs = path[1:].split("/")
+        bottom = self._find_bottom_node(dirs[:-1])
+        if dirs[-1] not in bottom.children:
+            raise ValueError("File does not exist")
+        else:
+            return bottom.children[dirs[-1]].contents
+
+    def delete_directory_or_file(self, path):
+        FileSystem._validate_path(path)
+        dirs = path[1:].split("/")
+        bottom = self._find_bottom_node(dirs[:-1])
+        if dirs[-1] not in bottom.children:
+            raise ValueError("File/Directory does not exist")
+        else:
+            bottom.delete_node(dirs[-1])
+
+    def size(self):
+        file_lst = []
+        path_to_go = [self.root]
+        result = 0
+        while len(path_to_go) > 0:
+            for i in path_to_go.pop(0).children.values():
+                if isinstance(i, Directory):
+                    path_to_go.append(i)
+                if isinstance(i, File):
+                    file_lst.append(i)
+        for i in file_lst:
+            result += len(i)
+        return result
+
+    def __str__(self):
+        return f"*** FileSystem ***\n" + self.root.__str__() + "\n***"
+    
+    @staticmethod
+    def _validate_path(path):
+        if not path.startswith("/"):
+            raise ValueError("Path should start with `/`.")
+
+    def _find_bottom_node(self, node_names):
+        curr_node = self.root
+        for name in node_names:
+            if not isinstance(curr_node, Directory) or name not in curr_node.children:
+                raise ValueError("Not a Directory or not found.")
+            curr_node = curr_node.children[name]
+        return curr_node
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name} ({type(self).__name__})"
+
+
+class Directory(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.children = {}
+
+    def add_node(self, node):
+        self.children[node.name] = node
+
+    def delete_node(self, name):
+        del self.children[name]
+
+    def __str__(self):
+        string = super().__str__()
+
+        children_strings = []
+        for child in list(self.children.values()):
+            child_string = child.__str__().rstrip()
+            children_strings.append(child_string)
+
+        children_combined_string = indent("\n".join(children_strings), 2)
+        string += "\n" + children_combined_string.rstrip()
+        return string
+
+
+class File(Node):
+    def __init__(self, name):
+        super().__init__(name)
+        self.contents = ""
+
+    def write_contents(self, contents):
+        self.contents = contents
 
     def __len__(self):
-        return len(self.cards)
-        
-    def shuffle(self):
-        r.shuffle(self.cards)
-    
-    def deal(self, n):
-        if n > len(self.cards):
-            return self.cards
-        return [self.cards.pop() for i in range(n)]
+        return len(self.contents)
 
-    def copy(self):
-        new = Deck()
-        new.cards = self.cards.copy()
-        return new
+    def __str__(self):
+        return super().__str__() + f" | {len(self)} characters"
 
-    def get_cards(self):
-        return self.cards.copy()
-    
-    def contains(self, card):
-        return card in self.cards
 
-    def sort_by_suit(self):
-        cards_by_suit = {"H": [], "D": [], "C": [], "S": []}
+def indent(string, number_of_spaces):
+    spaces = " " * number_of_spaces
+    lines = string.split("\n")
+    indented_lines = [spaces + line for line in lines]
+    return "\n".join(indented_lines)
 
-        for card in self.cards:
-            suit = card[-1]
-            cards_by_suit[suit].append(card)
-
-        self.cards = (
-            cards_by_suit["H"] +
-            cards_by_suit["D"] +
-            cards_by_suit["C"] +
-            cards_by_suit["S"]
-        )
                 
---
+++ oob/inventory.py
@@ -0,0 +1,38 @@
+class Inventory:
+    def __init__(self, max_capacity):
+        self.max_capacity = max_capacity
+        self.items = {}
+        self.item_count = 0
+
+    def add_item(self, name, price, quantity):
+        if self.item_count+quantity > self.max_capacity or name in self.items:
+            return False
+        self.item_count += quantity
+        self.items[name] = [name, price, quantity]
+        return True
+        
+
+    def delete_item(self, name):
+        if name not in self.items:
+            return False
+        self.item_count -= self.items[name][2]
+        del self.items[name]
+        return True
+
+    def get_items_in_price_range(self, min_price, max_price):
+        lst = []
+        for item in self.items.values():
+            if min_price <= item[1] <= max_price:
+                lst.append(item[0])
+        return lst
+
+
+    def get_most_stocked_item(self):
+        max_item_name = None
+        max_quant = 0
+        if self.item_count > 0:
+            for item in self.items.values():
+                if max_quant < item[2]:
+                    max_item_name = item[0]
+                    max_quant = item[2]
+        return max_item_name
